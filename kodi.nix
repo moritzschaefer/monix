a057ea072e7efcb78da6a2a3977a59e87d623951
Moritz Schaefer <mail@moritzs.de>
2022-06-06 18:33:25 +0200

    kodi/plex

---
+++ kodi.nix
@@ -0,0 +1,11 @@
+{ users, ... }:
+{
+  services.xserver.enable = true;
+  services.xserver.desktopManager.kodi.enable = true;
+  services.xserver.displayManager.autoLogin.enable = true;
+  services.xserver.displayManager.autoLogin.user = "kodi";
+
+  # Define a user account
+  users.extraUsers.kodi.isNormalUser = true;
+  nixpkgs.config.kodi.enableAdvancedLauncher = true;
+}
